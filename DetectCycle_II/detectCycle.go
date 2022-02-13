165ab2f35e9b20f79263135ab47b7d7ee75328c8
zhangyupeng <zhangyupeng@qutoutiao.net>
2022-02-13 14:38:10 +0800

    add new alg

--- DetectCycle_II/detectCycle.go
+++ DetectCycle_II/detectCycle.go
@@ -1,13 +1,26 @@
 package main
 
 type ListNode struct {
-	Val int
+	Val  int
 	Next *ListNode
 }
+
 func detectCycle(head *ListNode) *ListNode {
-	slow,fast := head,head
-	for slow != nil {
+	slow, fast := head, head
+	for fast != nil {
 		slow = slow.Next
-		
+		if fast.Next == nil {
+			return nil
+		}
+		fast = fast.Next.Next
+		if fast == slow {
+			p := head
+			for p != slow {
+				p = p.Next
+				slow = slow.Next
+			}
+			return p
+		}
 	}
+	return nil
 }
