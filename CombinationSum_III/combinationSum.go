24d0cfea0fc66b5356a976b4c615935c50c2003a
zhangyupeng <zhangyupeng@qutoutiao.net>
2022-02-14 22:22:15 +0800

    update file

--- CombinationSum_III/combinationSum.go
+++ CombinationSum_III/combinationSum.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// 有些问题
+// 组合总和iii
 func combinationSum(k, n int) [][]int {
 	result := [][]int{}
 	//max := 9
@@ -11,23 +11,16 @@ func combinationSum(k, n int) [][]int {
 		if sum > n || len(track) > k{
 			return
 		}
-		if sum == n && len(track) == k {
-			// 用于检验是否有重复的
-			m := map[int]struct{}{}
-			for _, v := range track {
-				m[v] = struct{}{}
-			}
-			if len(m) != k {
-				return
-			}
+		if  sum == n && len(track) == k {
 			tmp := make([]int, k)
 			copy(tmp, track)
 			result = append(result, tmp)
+			return
 		}
 		for i := start; i <= 9-(k-len(track))+1; i++ {
 			sum = sum + i
 			track = append(track, i)
-			backtracking(k, n, start + 1, sum, track)
+			backtracking(k, n, i + 1, sum, track)
 			sum = sum - i
 			track = track[:len(track)-1]
 		}
